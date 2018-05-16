e590b244d0039ef9114896332d2a6bcbf492786a
beany <mr_beany@163.com>
2018-05-16 21:00:18 +0800

    添加防止XSS攻击功能

---
+++ src/main/java/com/example/demo/core/configurer/XssFilterConfigurer.java
@@ -0,0 +1,37 @@
+package com.example.demo.core.configurer;
+
+import com.example.demo.core.filter.XssFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 张瑶
+ * @Description:xss过滤拦截器配置文件
+ * @time 2018/5/16 20:47
+ */
+@Configuration
+public class XssFilterConfigurer {
+
+    /**
+     * xss过滤拦截器
+     */
+    @Bean
+    public FilterRegistrationBean xssFilterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new XssFilter());
+        filterRegistrationBean.setOrder(Integer.MAX_VALUE-1);
+        filterRegistrationBean.setEnabled(true);
+        filterRegistrationBean.addUrlPatterns("/*");
+        Map<String, String> initParameters = new HashMap();
+        //excludes用于配置不需要参数过滤的请求url
+        initParameters.put("excludes", "/favicon.ico,/img/*,/js/*,/css/*");
+        //isIncludeRichText主要用于设置富文本内容是否需要过滤
+        initParameters.put("isIncludeRichText", "true");
+        filterRegistrationBean.setInitParameters(initParameters);
+        return filterRegistrationBean;
+    }
+}
---
+++ src/main/java/com/example/demo/core/filter/XssFilterUtil.java
@@ -0,0 +1,34 @@
+package com.example.demo.core.filter;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Whitelist;
+
+/**
+ * @author 张瑶
+ * @Description: xss非法标签过滤
+ * @date 2018/5/16 20:03
+ */
+public class XssFilterUtil {
+
+    /**
+     * 使用自带的basicWithImages 白名单
+     * 允许的便签有a,b,blockquote,br,cite,code,dd,dl,dt,em,i,li,ol,p,pre,q,small,span,
+     * strike,strong,sub,sup,u,ul,img
+     * 以及a标签的href,img标签的src,align,alt,height,width,title属性
+     */
+    private static final Whitelist whitelist = Whitelist.basicWithImages();
+
+    /** 配置过滤化参数,不对代码进行格式化 */
+    private static final Document.OutputSettings outputSettings = new Document.OutputSettings().prettyPrint(false);
+    static {
+        // 富文本编辑时一些样式是使用style来进行实现的
+        // 比如红色字体 style="color:red;"
+        // 所以需要给所有标签添加style属性
+        whitelist.addAttributes(":all", "style");
+    }
+
+    public static String clean(String content) {
+        return Jsoup.clean(content, "", whitelist, outputSettings);
+    }
+}
